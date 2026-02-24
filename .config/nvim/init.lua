aad20786a74151f202024568f23fd6e3739d0ea7
BizkitKing <94636629+BizkitKing@users.noreply.github.com>
2026-02-24 16:21:17 +0100

    1 file nvim 0.12 change
    from sylan franklin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,44 @@
-require("config.options")
-require("config.keybinds")
-require("config.lazy")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+
+
+vim.pack.add({
+		{src = "https://github.com/vague2k/vague.nvim"},
+		{src = "https://github.com/stevearc/oil.nvim"},
+		{src = "https://github.com/echasnovski/mini.pick"},
+		{src = "https://github.com/neovim/nvim-lspconfig"},
+		{src = "https://github.com/chomosuke/typst-preview.nvim"},
+
+})
+
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+
+
+--lsp
+vim.lsp.enable({"lua_ls"}, {"clangd"})
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+
+
+
+-- looks
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
